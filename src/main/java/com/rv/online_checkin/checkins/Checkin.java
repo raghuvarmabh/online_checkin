7b6dd0a0bef0e16a85c84a25ea4086cdeae7a52c
Raghu Bhupatiraju <raghuvarma.bhr@gmail.com>
2019-12-16 22:23:29 -0800

    setting up test data source

--- src/main/java/com/rv/online_checkin/checkins/Checkin.java
+++ src/main/java/com/rv/online_checkin/checkins/Checkin.java
@@ -14,73 +14,73 @@ import org.springframework.beans.factory.annotation.Value;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-
 @Entity
-@Table(name="checkins")
+@Table(name = "checkins")
 public class Checkin {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
-  
+
   @NotBlank(message = "Name is required")
   @Size(max = 50)
   private String name;
-  
+
   @NotBlank(message = "Phone number is required")
-  @Size(max = 10, message= "Phone number should be 10 numbers")
-  @Size(min = 10, message= "Phone number should be 10 numbers")
+  @Size(max = 10, message = "Phone number should be 10 numbers")
+  @Size(min = 10, message = "Phone number should be 10 numbers")
   private String phone_number;
-  
-  private LocalDateTime currentDateTime = LocalDateTime.now(); 
-  
-  @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+
+  private LocalDateTime currentDateTime = LocalDateTime.now();
+
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private LocalDateTime checkedInAt = LocalDateTime.now();
 
-  @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private LocalDateTime updatedAt = LocalDateTime.now();
-  
+
+  public Checkin(String name, String phone_number) {
+    this.name = name;
+    this.phone_number = phone_number;
+  }
+
   public Long getId() {
-	return id;
-}
+    return id;
+  }
 
-public void setId(Long id) {
-	this.id = id;
-}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-public String getName() {
-	return name;
-}
+  public String getName() {
+    return name;
+  }
 
-public void setName(String name) {
-	this.name = name;
-}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-public String getPhone_number() {
-	return phone_number;
-}
+  public String getPhone_number() {
+    return phone_number;
+  }
 
-public void setPhone_number(String phone_number) {
-	this.phone_number = phone_number;
-}
+  public void setPhone_number(String phone_number) {
+    this.phone_number = phone_number;
+  }
 
-public LocalDateTime getCheckedInAt() {
-	return checkedInAt;
-}
+  public LocalDateTime getCheckedInAt() {
+    return checkedInAt;
+  }
 
-public void setCheckedInAt(LocalDateTime checkedInAt) {
-	this.checkedInAt = checkedInAt;
-}
+  public void setCheckedInAt(LocalDateTime checkedInAt) {
+    this.checkedInAt = checkedInAt;
+  }
 
-public LocalDateTime getUpdatedAt() {
-	return updatedAt;
-}
+  public LocalDateTime getUpdatedAt() {
+    return updatedAt;
+  }
 
-public void setUpdatedAt(LocalDateTime updatedAt) {
-	this.updatedAt = updatedAt;
-}
-
-
-
-
+  public void setUpdatedAt(LocalDateTime updatedAt) {
+    this.updatedAt = updatedAt;
+  }
 
 }
